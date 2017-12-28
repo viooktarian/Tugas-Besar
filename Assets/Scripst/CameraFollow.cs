960f372291afd00b74a9b866fbd4d3a5a098ab65
viooktarian <viooktarian001@gmail.com>
2017-12-29 00:37:32 +0700

    Tugas-Besar

---
+++ Assets/Scripst/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	[SerializeField] Transform Target;
+	[SerializeField] float followSpeed;
+	Vector3 offset;
+
+	void Start(){
+		offset = Target.position - transform.position;
+	}
+
+	void FixedeUpdate(){
+		transform.position = Vector3.Lerp (transform.position, Target.position - offset, Time.fixedDeltaTime * followSpeed);
+	}
+}
---
+++ Assets/Scripst/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Rigidbody myRigidbody;
+	[SerializeField] float moveSpeed;
+	[SerializeField] float jumpPower;
+	float hInput;
+	float vInput;
+	bool jumpInput;
+
+	void Awake(){
+		myRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void Update(){
+		hInput = Input.GetAxis ("Horizontal") * moveSpeed;
+		vInput = Input.GetAxis ("Vertical") * moveSpeed;
+		jumpInput = Input.GetKeyDown (KeyCode.Space);
+	}
+
+	void FixedUpdate(){
+		Move ();
+		if (jumpInput) {
+			Jump ();
+			jumpInput = false; 
+		}
+	}
+
+	void Move(){
+		myRigidbody.AddForce (hInput * Time.fixedDeltaTime, 0f, vInput * Time.fixedDeltaTime);
+	}
+
+	void Jump(){
+		myRigidbody.AddForce (Vector3.up * jumpPower * Time.fixedDeltaTime, ForceMode.Impulse);
+	}
+}
